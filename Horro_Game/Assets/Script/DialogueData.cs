5a6f45825a7867c7c3c58169d81a6e8626f26e1b
hinying <lisawu3032117@gmail.com>
2023-01-09 02:57:50 +0800

    場景

---
+++ Horro_Game/Assets/Script/DialogueData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace HSIN
+{
+
+    /// <summary>
+    /// 對話資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "HSIN/Dialogue Data", fileName = "New Dialogue Date")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話者內容"), TextArea(2, 10)]
+        public string[] dialogueContent;
+
+    }
+
+}
+
---
+++ Horro_Game/Assets/Script/LearnLoop.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+namespace HSIN
+{
+    /// <summary>
+    /// 學習迴圈，重複執行程式
+    /// for、while、do while、foreach
+    /// </summary>
+    public class LearnLoop : MonoBehaviour
+    {
+        private void Awake()
+        {
+
+            //for 迴圈語法
+            //for (初始值; 布林值/條件; 迴圈結束執行) {程式}
+            for (int i = 0; i < 10; i++)
+            {
+                print("for 迴圈：" + i);
+            }
+
+            for (int number = 0; number < 5; number++)
+            {
+                print("迴圈：" + number);
+            }
+
+            if (true)
+            {
+                print("判斷式");
+            }
+
+            int count = 0;
+
+            while (count < 5)
+            {
+                print("()內的布林值為 true 就會持續執行。");
+                print("while 迴圈數字：" + count);
+                count++;
+
+            }
+
+
+        }
+
+    }
+}
+
